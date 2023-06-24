31cbc7f91d76f78b474914e13aa780682e337bbd
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-06-24 22:46:55 +0300

    added ssl files path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: readFileSync('/etc/ssl/private/private.key'),
+      cert: readFileSync('/etc/ssl/certs/certificate.crt'),
+    },
+  });
   await app.listen(11000);
 }
 bootstrap();
