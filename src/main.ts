c0245d019dd3ffe808970b05100ff43380171105
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-07-22 16:03:18 +0300

    removed ssl file location

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const checkPrivateKey = fs.existsSync('/etc/ssl/private/private.key');
-  const checkCertificate = fs.existsSync('/etc/ssl/certs/certificate.crt');
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions:
-      checkPrivateKey && checkCertificate
-        ? {
-            key: readFileSync('/etc/ssl/private/private.key'),
-            cert: readFileSync('/etc/ssl/certs/certificate.crt'),
-          }
-        : undefined,
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(11000);
 }
 bootstrap();
